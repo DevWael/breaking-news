1e0aa7402b15673534015a3b5e51c4837a7d8e8b
devwael <dev.ahmedwael@gmail.com>
2022-08-01 23:42:15 +0200

    General: define path dir and uri constants

--- ibn.php
+++ ibn.php
@@ -36,6 +36,8 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'IBN_VERSION', '1.0.0' );
+define( 'IBN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'IBN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 /**
  * The code that runs during plugin activation.
