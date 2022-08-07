29705390ee324d67397ce1c2369f4fd7bd2f639d
devwael <dev.ahmedwael@gmail.com>
2022-08-07 20:07:12 +0200

    Add documentation file

--- admin/js/block-editor/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'df1bfb7252df746682f5');
--- admin/js/block-editor/src/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@wordpress/plugins';
-import render from './metabox';
-
-registerPlugin( 'ibn-breaking-news-metabox', {
-	icon: 'visibility',
-	render,
-} );
